5ba35c3d2b776108d9fff89e5c95e8463d3604dc
TheMacDevTeam <themacdevteam@gmail.com>
2020-05-08 12:14:55 +1200

    nav fix plus our-track content

--- website/nav_mobile_script.js
+++ website/nav_mobile_script.js
@@ -2,6 +2,14 @@
 
 function navMobile() {
 	var x = document.getElementById("mobile_links");
+	if (document.getElementById("wowslider-container0")) {
+		var y = document.getElementById("wowslider-container0")
+		if (x.style.display === "block") {
+			y.style.display = "block";
+		} else {
+			y.style.display = "none";
+		}
+	}
 	if (x.style.display === "block") {
 		x.style.display = "none";
 	} else {

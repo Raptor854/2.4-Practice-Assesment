4bdb4f7818267c0119e3711894d82504dd180e1d
TheMacDevTeam <themacdevteam@gmail.com>
2020-05-19 13:10:05 +1200

    Mobile Nav

--- website/nav_mobile_script.js
+++ website/nav_mobile_script.js
@@ -2,6 +2,7 @@
 
 function navMobile() {
 	var x = document.getElementById("mobile_links");
+	/*
 	if (document.getElementById("wowslider-container0")) {
 		var y = document.getElementById("wowslider-container0")
 		if (x.style.display === "block") {
@@ -9,10 +10,15 @@ function navMobile() {
 		} else {
 			y.style.display = "none";
 		}
-	}
-	if (x.style.display === "block") {
-		x.style.display = "none";
+	}*/
+	
+	var z = document.getElementById("nav_spacer");
+	if (x.style.height === '197px') {
+		x.style.height = '0px';
+		z.style.height = "0px";
+		setTimeout(function() {x.style.display = "none";},500)
 	} else {
 		x.style.display = "block";
+		setTimeout(function() {x.style.height = '197px';z.style.height = "197px";},0)
 	}
 }
